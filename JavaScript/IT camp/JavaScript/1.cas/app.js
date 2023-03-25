cff4edf72517b170a6af907f04d933aade673fa7
dzanafeta <dzanafetahovic@gmail.com>
2023-03-25 11:34:54 +0100

    Tipovi podataka- drugi deo

---
+++ JavaScript/IT camp/JavaScript/1.cas/app.js
@@ -0,0 +1,96 @@
+console.log("Prvi cas javascripta");
+console.log("nova poruka");
+
+tekst = document.getElementById("paragraf");
+console.log(tekst);
+
+tekst.innerHTML = "<em>Da li se vidi paragraf?<em>";
+
+//  TIPOVI PODATAKA //
+
+//  Postoje:
+// 1. primitivni (vrednosni) tipovi podataka i
+// 2. neprimitivni(referentni) tipovi podataka.
+
+//  Za proveru tipa odredjene promenljive ili (varijable) se koristi typeof operator.
+
+// PRIMIR=TIVNI(VREDNOSNI) TIPOVI PODATAKA:
+
+// 1. string - promenljiva koja je zapisana unutar navodnika (obicnih ili duplih);
+
+console.log("neki string");
+console.log(typeof "neki string");
+
+// 2.number - brojevi mogu biti zapisani sa ili bez decimalnog zareza (.)
+
+console.log(56);
+console.log(typeof 56);
+
+console.log(56.56);
+// console.log(typeoff 56.56);
+
+// 3.BigInt - brojevi koji su iznad ogranicenja za number tip podatka.
+
+console.log(BigInt(566328695698326598326583));
+console.log(typeof BigInt(566328695698326598326583));
+
+// 4. boolean - Logicki entitet koji ima dve vrednosti ;
+// true;
+// false
+
+console.log(true);
+console.log(typeof true);
+
+// 5. undefined - Promenljiva koja je definisana u memoriji ali joj nije dodeljena vrednsosti ima:
+// tip: undefied i
+// vrednost: undefied.
+
+var a;
+
+console.log(a);
+console.log(typeof a);
+
+// 6. null - Nepostojeca ili nekorektna promeljiva ima vrednost null.
+// Tip promenljive cija je vrednost null u JavaScriptu je object.
+
+b = null;
+
+console.log(b);
+console.log(typeof b);
+
+// 7. Symbol - Koristi se za promenljive koje su anonimne ili jeinstvene.
+
+c = Symbol("IT CAMP");
+console.log(c);
+console.log(typeof c);
+
+// Svi tipovi koji nisu primitivni su tipa object.
+// Promenljive koje mogu sadrzati vise (od jedne) vrednosti su referentnog (neprimitivnog) tipa.
+
+// 1. Array (niz) - Struktura podataka za skladistenje vise vrednosti ( te vrednsoti mogu biti razlivitog tipa).
+
+niz = ["Emir, 21, true"];
+console.log(niz);
+console.log(typeof niz);
+
+// 2. Object( objekat) - Struktura podataka za skladistenje vise vrednosti (te vrednosti mogu biti razlicitog tipa).
+// Te vrednosti su prikazane u key:value (name:value) paru.
+obj = { ime: "Emir", prezime: "Marukic", punoletan: true };
+console.log(obj);
+console.log(typeof obj);
+
+// 3. Set - Struktura podataka za skladistenje vise vrednosti koja ne dozvoljava duplikate. Slicna je nizu (Array), a sintaksa za pravljenje seta je:
+
+set = new Set(["Asija", "Melida", 19, 31, 25, "Dino"]);
+// Svako novo pojavljivanje u setu se nece primeniti.
+console.log(set);
+
+// 4. Map- Struktura podataka za skladistenje vrednosti i slicna je objektu. Sintaksa za pravljenje mape je:
+
+map = new Map([
+  ["banana", 160],
+  ["jagoda", 240],
+  ["kruska", 140],
+  ["ananas", 289],
+]);
+console.log(map);
