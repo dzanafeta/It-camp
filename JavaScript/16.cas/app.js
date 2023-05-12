64ad1b54d5ff5a54003c80d098d1a93528565ec6
dzanafeta <dzanafetahovic@gmail.com>
2023-05-12 20:24:12 +0200

    new files

---
+++ JavaScript/16.cas/app.js
@@ -0,0 +1,20 @@
+const prom1 =
+  "Sta ako zelimo da se data recenica ispise u \n dva odvojena reda";
+console.log(prom1);
+
+const prom2 =
+  "Sta ako zelimo da se data recenica ispise u dva odvojena reda kada kucamo,  ali njen prikaz ce izgledati u jednom redu";
+console.log(prom2);
+
+let br1 = 7;
+let br2 = 13;
+let zbir = br1 + br2;
+console.log("Zbir brojeva" + 7 + "i" + 13 + "je" + (br1 + br2));
+
+// Resenje preko back- ticks
+
+console.log("Zbir brojeva ${br1} i ${br2} je ${br1 + br2}");
+
+//  Zapisivanje navodnika se vrsi bez ikakvih problema kroz back-ticks sintaksu.
+
+// Zapisivanje stringa u vise redova je ispravna sintaksa kroz back- ticks navode.
