dbeac95f4190d49909d926bc5a8292404d956268
dzanafeta <dzanafetahovic@gmail.com>
2023-06-23 20:55:15 +0200

    metode splice slice conact za niz

--- JavaScript/34.cas/app.js
+++ JavaScript/34.cas/app.js
@@ -36,5 +36,62 @@ console.log(celaGrupa);
 
 // Primenom splice metode izbrisati sve elemente sve devojke, zatim ih dodati na pocetak niza.
 
-celaGrupa.splice(2, 5);
+//splice() metoda vraca niz sa izbrisanim elementima
+const devojke2 = celaGrupa.splice(2, 5);
 console.log(celaGrupa);
+
+console.log(celaGrupa);
+
+const novaGrupa = devojke2.concat(celaGrupa);
+console.log(novaGrupa);
+novaGrupa.splice(5, 0, "Ajla");
+
+novaGrupa.splice(5, 2, "Dzevdet", "Tarik", "Marija", "Alem");
+console.log(novaGrupa);
+
+novaGrupa.splice(-3, 1, "Melida");
+console.log(novaGrupa);
+
+// sort() metoda vrsi sortiranje niza (abecedno)
+
+novaGrupa.sort();
+console.log(novaGrupa);
+
+//reverrse() metoda sortira niz od poslednjeg ka prvom elementu.
+
+//Za sortiranje niza od Z do A prvo treba iskoristiti sort() metodu, pa onda
+//reverse();
+novaGrupa.sort();
+novaGrupa.reverse();
+console.log(novaGrupa);
+
+//// sortiranje niz brojeva samo po sebi se ne vrsi kako bismo mi hteli:
+const brojevi = [3, 4, -99, 23, 32];
+brojevi.sort();
+console.log(brojevi);
+
+//Potrebno je poslati funkciju koja vrsi poredjenje dva broja kako bimo
+//sortirali niz od najveceg ka najmanjem elementu:
+brojevi.sort(function (a, b) {
+  return a - b;
+});
+console.log(brojevi);
+
+//Potrebno je poslati funkciju koja vrsi poredjenje dva broja kako bimo
+//sortirali niz od najveceg ka najmanjem elementu:
+brojevi.sort(function (a, b) {
+  return b - a;
+});
+
+//brojevi.sort((a,b) => b - a);
+console.log(brojevi);
+
+//Sortiranje niza u random redosledu:
+
+brojevi.sort(function () {
+  return 0.5 - Math.random();
+});
+console.log(brojevi);
+
+//1.Napraviti funkciju koja vraca najveci element nekog niza.
+//2.Napraviti funkciju koja vraca najmanji element nekog niza.
