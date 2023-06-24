aace64a4318feca9eac484c2d5a6e23083eda39d
dzanafeta <dzanafetahovic@gmail.com>
2023-06-24 23:46:19 +0200

    domaci

---
+++ JavaScript/34.cas/domaci.js
@@ -0,0 +1,8 @@
+// 1. Napraviti funkciju koja vraca najveci element nekog niza.
+// 2. Napraviti funkciju koja vraca najmanji element nekog niza.
+// 3. Iz naseg niza grupa napraviti dva niza muskarci i devojke, s tim sto se Dzevdet, Marija, Melida i
+//Tarik ne smeju naci.
+// 4. Napraviti funkciju koja pravi novi niz, koji ce sadrzati samo elemente
+// koji su se nalazili na neparnim mestima unutar originalnog niza.
+// [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+// [1,3,5,7,9,11,13]
