c685ceb2df69fbfe3bff135272190c020c4fb9e8
dzanafeta <dzanafetahovic@gmail.com>
2023-07-14 20:53:17 +0200

    array metode

---
+++ JavaScript/35.cas/domaci.js
@@ -0,0 +1,19 @@
+// Domaci:
+// 1. Napraviti funkciju koja pravi novi niz, koji ce sadrzati samo elemente koji su se nalazili
+// na parnim mestima unutar originalnog niza.
+// [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+// [2,4,6,8,10,12,14]
+// 2. Iz nekog niza izdvojiti sve parne brojeve.(Napraviti novi niz)
+// 3. Iz nekog niza izdvojiti sve neparne brojeve.(Napraviti novi niz)
+// 4. Iz nekog niza izdvojiti sve pozitivne brojeve.(Napraviti novi niz)
+
+// 5. Za dati niz izvrsiti:
+// Sortiranje niza od najveceg ka najmanjem broju,
+// Sortiranje niza od najmanjeg ka najvecem broju.
+
+// 6. Write a simple JavaScript program to join all elements of the following array
+// into a string where is * between elements.
+
+// 1.
+
+const niz = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
---
+++ JavaScript/41.cas/domaci.js
@@ -0,0 +1,30 @@
+// 3.Broj palindrom se cita isto sa obe strane. Najveci palindrom napravljen od proizvoda
+//dva dvocifrena broja je 9009 = 91 x 99.
+
+//Pronaci najveci palindrom napravljen od proizvodaa dva trocifrena broja.
+
+//"123"
+//"321"
+
+function jeLiPalindrom(broj) {
+  const strBroj = broj.toString();
+  let obrnutiBroj = "";
+  for (let i = strBroj.length - 1; i >= 0; i--) {
+    obrnutiBroj += strBroj[i];
+  }
+}
+
+// if (strBroj === obrnutiBroj){
+
+function najveciPalindrom() {
+  let najveciPalindrom = 0;
+  for (let i = 999; i > 99; i--) {
+    for (let j = i; j > 99; j--) {
+      if (jeLiPalindrom(i * j) && i * j > najveciPalindrom) {
+        najveciPalindrom = i * j;
+      }
+    }
+  }
+  return najveciPalindrom;
+}
+console.log(najveciPalindrom());
